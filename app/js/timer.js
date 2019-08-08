5de951819a1c7f529c960f6afd6bf0d132e8e1c8
ViktoriaZaharova <zaharova181114@gmail.com>
2019-08-08 19:58:35 +0300

    timer

--- app/js/timer.js
+++ app/js/timer.js
@@ -1,83 +1,41 @@
-// Працює для будь-якої кількості таймерів на сторінці + для модальних вікон.
-// Задати для тегів відповідні класси "hours", "minutes", "seconds".
-
-// Для сторінок, де час відображається порозрядно, вказуємо different = true (15 рядок).
-// Приклад image.prntscr.com/image/TzEcLkY_T_SiX5GrLlIvdw.png
-
-// Для очищення localStorage в консолі браузера прописуємо localStorage.clear();
-
-document.addEventListener('DOMContentLoaded', function () {
-    if (!localStorage.getItem("heytimer")) {
-        var time = {
-            days: 0,            // Кількість днів;
-            hours: 23,           // Кількість годин;
-            minutes: 59,        // Кількість хвилин;
-            seconds: 45          // Кількість секунд;
-        }, different = false;
-
-        time = time.days * 86400 + time.hours * 3600 + time.minutes * 60 + time.seconds;
-
-
-        localStorage.setItem("time", time);
-        localStorage.setItem("heytimer", true);
-        localStorage.setItem("different", different);
-    }
-    hellotimer();
-});
-
-function hellotimer() {
-    var time = localStorage.getItem('time'),
-        different = localStorage.getItem('different') === "true",
-        days = parseInt(time / 86400, 10),
-        hours = parseInt(time % 86400 / 3600, 10),
-        minutes = parseInt((time - (days * 86400 + hours * 3600) ) / 60, 10),
-        seconds = parseInt(time % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes :  "" + minutes;
-    seconds = seconds < 10 ? "0" + seconds :  "" + seconds;
-    hours = hours < 10 ? "0" + hours :  "" + hours;
-    days = days < 10 ? "0" + days : "" + days;
-
-    if (--time < 0) {
-        return;
-    }
-
-    var daysHTML = document.getElementsByClassName("days");
-    var hoursHTML = document.getElementsByClassName("hours");
-    var minutesHTML = document.getElementsByClassName("minutes");
-    var secondsHTML = document.getElementsByClassName("seconds");
-
-    if (different) {
-        seconds = seconds.split("");
-        minutes = minutes.split("");
-        hours = hours.split("");
-        days = days.split("");
-
-        doubleFilling(daysHTML, days);
-        doubleFilling(hoursHTML, hours);
-        doubleFilling(minutesHTML, minutes);
-        doubleFilling(secondsHTML, seconds);
-    } else {
-        filling(daysHTML, days);
-        filling(hoursHTML, hours);
-        filling(minutesHTML, minutes);
-        filling(secondsHTML, seconds);
-    }
-
-    localStorage.setItem("time", time);
-    setTimeout(hellotimer, 1000);
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    // var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        // 'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
 }
 
-function filling(obj, value) {
-    for (var i = 0; i < obj.length; i++) {
-        obj[i].innerHTML = value;
-    }
-};
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    // var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
 
-function doubleFilling(obj, value) {
-    for (var i = 0; i < obj.length; i++) {
-        obj[i].innerHTML = value[i % 2];
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        // daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
     }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
 }
 
-// Для відміни записку localStorage, закоментувати 21 рядок localStorage.setItem("heytimer", true);
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
+initializeClock('countdown', deadline);
